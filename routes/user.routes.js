8ccde99e67bf5cc4e7469aacd0bae4ca079191c8
jmcob <77777490+jmcob@users.noreply.github.com>
2022-04-04 10:24:18 +0200

    password validator added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const userCtrl = require("../controllers/user.ctrl");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+const passwordCheck = require("../middleware/password");
 
 // route for login and signup : user.
-router.post("/signup", userCtrl.signup);
+router.post("/signup", passwordCheck, userCtrl.signup);
 router.post("/login", userCtrl.login);
 router.get("/:id", userCtrl.getOne);
 router.put("/:id", auth, multer, userCtrl.updateOne);
