e93fb61e8039032baa7cac2dbacd0eaad38445e6
jmcob <77777490+jmcob@users.noreply.github.com>
2022-04-05 16:24:21 +0200

    commit avec faute dans auth pour les formdata et admin = false

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,11 +7,20 @@ module.exports = (req, res, next) => {
         const decodedToken = jwt.verify(token, process.env.JWT);
         const user_id = decodedToken.user_id;
         const admin = decodedToken.admin;
-        if (req.body.user_id && req.body.user_id !== user_id) {
-            throw "Invalid user ID";
-        } else if (admin === true) {
+        if (admin === true) {
             next();
-        } else if (req.body.user_id && req.body.user_id === user_id) next();
+        } else if (req.body.user_id && req.body.user_id === user_id) {
+            next();
+        } else if (
+            JSON.parse(req.body.user.user_id) &&
+            JSON.parse(req.body.user.user_id) == user_id
+        ) {
+            next();
+        } else if (req.body.post.user_id && req.body.post.user_id == user_id) {
+            next();
+        } else {
+            throw "Invalid user ID";
+        }
     } catch {
         res.status(401).json({
             error: new Error("Invalid request!"),
