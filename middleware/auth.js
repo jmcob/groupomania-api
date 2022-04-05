9fe2fa76560fde03c427ea80d8bb50bed973f588
jmcob <77777490+jmcob@users.noreply.github.com>
2022-04-05 13:47:28 +0200

    maj auth admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,10 +6,12 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, process.env.JWT);
         const user_id = decodedToken.user_id;
+        const admin = decodedToken.admin;
         if (req.body.user_id && req.body.user_id !== user_id) {
             throw "Invalid user ID";
-        }
-        next();
+        } else if (admin === true) {
+            next();
+        } else if (req.body.user_id && req.body.user_id === user_id) next();
     } catch {
         res.status(401).json({
             error: new Error("Invalid request!"),
